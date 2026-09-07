aba87cbdd2b86ccb70f6a0df0ce180b57dca21bc
kumarram27 <115088025+kumarram27@users.noreply.github.com>
2026-09-08 00:26:09 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
+    qualities: [50, 75],
+
     remotePatterns: [
       {
         protocol: "https",
